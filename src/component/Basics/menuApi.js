cdd6d90b42139b0140f935036570d09a133c5452
PraveenKumarJha123 <99195632+PraveenKumarJha123@users.noreply.github.com>
2023-07-09 13:02:22 +0530

    Update menuApi.js
    updated

--- src/component/Basics/menuApi.js
+++ src/component/Basics/menuApi.js
@@ -1,86 +1,87 @@
 const Menu = [
-    {
-      id: 1,
-      image: "images/maggi.jpg",
-      name: "maggi",
-      category: "breakfast",
-      price: "12₹",
-      description:
-        "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
-    },
-  
-    {
-      id: 2,
-      image: "images/allupakoida.jpg",
-      name: "allu pakoida",
-      category: "evening",
-      price: "20₹",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
-    },
-    {
-      id: 3,
-      image: "images/corn.jpg",
-      name: "corn",
-      category: "breakfast",
-      price: "10₹",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
-    },
-    {
-      id: 4,
-      image: "../images/chola.jpg",
-      name: "chola",
-      category: "lunch",
-      price: "50₹",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
-    },
-    {
-      id: 5,
-      image: "../images/pizza.jpg",
-      name: "pizza",
-      category: "evening",
-      price: "80₹",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
-    },
-    {
-      id: 6,
-      image: "../images/nonvegthali.jpg",
-      name: "Non-Veg Thali",
-      category: "dinner",
-      price: "180₹",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
-    },
-    {
-      id: 7,
-      image: "../images/sweet.jpg",
-      name: "Sweets",
-      category: "dinner",
-      price: "60₹",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
-    },
-    {
-      id: 8,
-      image: "../images/rajmarice.jpg",
-      name: "Rajma Rice",
-      category: "lunch",
-      price: "60₹",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
-    },
-    {
-      id: 9,
-      image: "../images/samosa.jpg",
-      name: "samaso",
-      category: "evening",
-      price: "10₹",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
-    },
-  ];
-  
-  export default Menu;
+  {
+    id: 1,
+    image: "images/maggi.jpg",
+    name: "maggi",
+    category: "breakfast",
+    price: "12₹",
+    description:
+      "Maggi noodles are dried noodles fused with oil, and sold with a packet of flavorings. These noodles are usually eaten after being cooked in boiling water for 3 to 5 minutes or eaten straight from the packet. Just boil the water, add the tastemaker and noodles and your maggi is ready within 2 to 3 minutes. ",
+  },
+
+  {
+    id: 2,
+    image: "images/allupakoida.jpg",
+    name: "allu pakoida",
+    category: "evening",
+    price: "20₹",
+    description:
+      "Aloo pakora are soft melt-in-the-mouth fried potato fritters coated in a delicious, golden, crispy batter. It is also known as potato bajji and has always been one of my favorite tea-time snacks. Make this awesome vegan potato snack in less than 30 minutes. ",
+  },
+  {
+    id: 3,
+    image: "images/corn.jpg",
+    name: "corn",
+    category: "breakfast",
+    price: "10₹",
+    description:
+      " A plant that produces large grains, or kernels, set in rows on a cob. Its many varieties produce numerous products, highly valued for both human and livestock consumption. Teosinte: Corn comes from this wild grass plant. Yield: The amount of a certain crop that is produced on a farm. ",
+  },
+  {
+    id: 4,
+    image: "../images/chola.jpg",
+    name: "chola",
+    category: "lunch",
+    price: "50₹",
+    description:
+      "It is one of the most popular vegetarian curry dishes in India. It is a chickpea curry cooked with onions, tomatoes and lots of spices. Chole is one of those core curries in northern India without which no party or wedding festivities is complete.  ",
+  },
+  {
+    id: 5,
+    image: "../images/pizza.jpg",
+    name: "pizza",
+    category: "evening",
+    price: "80₹",
+    description:
+      "A balanced ratio of tomato, cheese, basil and drizzle of olive oil would make a good piece of pizza.” The classic Pizza Margherita served at Atlas Pizzeria. ",
+  },
+  {
+    id: 6,
+    image: "../images/nonvegthali.jpg",
+    name: "Non-Veg Thali",
+    category: "dinner",
+    price: "180₹",
+    description:
+      "Coming to the recipe,its a simple roast can be made with boneless chicken.The highlight of the recipe is,its doesn't need onion and tomato.All you need to do is just marinate the chicken with the spices for 2-3 hours and cook it.",
+      
+  },
+  {
+    id: 7,
+    image: "../images/sweet.jpg",
+    name: "Sweets",
+    category: "dinner",
+    price: "60₹",
+    description:
+      "Sugar confectionery includes candies (also called sweets, short for sweetmeats, in many English-speaking countries), candied nuts, chocolates, chewing gum, bubble gum, pastillage, and other confections that are made primarily of sugar. ",
+  },
+  {
+    id: 8,
+    image: "../images/rajmarice.jpg",
+    name: "Rajma Rice",
+    category: "lunch",
+    price: "60₹",
+    description:
+      "Rajma is large with dark red in colour and due to the unique shape of a kidney, they are also called kidney beans. They take a long time to cook and are usually used to cook soups as they have a firm texture and hold up well. ",
+  },
+  {
+    id: 9,
+    image: "../images/samosa.jpg",
+    name: "samaso",
+    category: "evening",
+    price: "10₹",
+    description:
+      "There is a some sourness in this stuffing as dry mango powder or dry pomegranate powder are added. A few more spices are added as well. In some variations, raisins are also added which gives the samosa a sweet-sour taste. ",
+  },
+];
+
+export default Menu;
